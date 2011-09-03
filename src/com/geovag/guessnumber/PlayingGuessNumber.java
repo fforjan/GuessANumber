e9d8f7ec500f79944182e0216656e56d0c4c9cdf
Frederic Forjan <fforjan@free.fr>
2011-09-03 16:23:02 +1000

    working game :)

--- src/com/geovag/guessnumber/PlayingGuessNumber.java
+++ src/com/geovag/guessnumber/PlayingGuessNumber.java
@@ -1,9 +1,15 @@
 package com.geovag.guessnumber;
 
+import java.util.Random;
+
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PlayingGuessNumber extends ListActivity {
 	
@@ -13,13 +19,59 @@ public class PlayingGuessNumber extends ListActivity {
 	
 	}
 	
+	private static Random randomizer = new Random();
+	
+	public int guessedNumber;
+	
+	private EditText proposedNumber;
+	private Button proposeButton;
+	
+	private ArrayAdapter<String> results;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.playing);
+		
+		//TODO : handle to saved instance state..
+		guessedNumber = randomizer.nextInt(1001);
+	
+		proposedNumber = (EditText) findViewById(R.id.proposedNumber);
+		results = new ArrayAdapter<String>(this,R.layout.guessed_row );
+		setListAdapter(results);
+		
+		proposeButton = (Button) findViewById(R.id.proposeButton);
+		proposeButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				newProposal();
+			}
+		});
 	}
 
+	private void newProposal()
+	{
+		int proposal = Integer.parseInt(proposedNumber.getText().toString());
+		if(proposal == guessedNumber)
+		{
+			results.insert("Win!", 0);
+			proposedNumber.setEnabled(false);
+			proposeButton.setEnabled(false);
+			
+		}
+		else if (proposal < guessedNumber)
+		{
+			results.insert(String.format("Guessed number is greather than %1$d",proposal),0);
+		}
+		else
+		{
+			results.insert(String.format("Guessed number is lesser than %1$d",proposal),0);
+		}
+		
+		proposedNumber.setText("");
+	}
 	
 	
 	@Override

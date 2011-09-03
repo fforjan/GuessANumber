b3db039e0f65f78a6502f7524946c323954db7c5
Frederic Forjan <fforjan@free.fr>
2011-09-03 14:11:51 +1000

    Initial import
    Signed-off-by: Frederic Forjan <fforjan@free.fr>

---
+++ src/com/geovag/guessnumber/WelcomeGuessNumber.java
@@ -0,0 +1,13 @@
+package com.geovag.guessnumber;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomeGuessNumber extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
